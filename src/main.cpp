2555ac4605adb37caee879a82803ff9ec133339d
Jeremy <slurpchunk7@gmail.com>
2026-06-08 14:33:07 +1000

    Improved rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include "ramfb.h"
 #include "Settings.h"
+#include "text.h"
+#include "pci.h"
+#include "print.h"
+
+
 #include <stdint.h>
 
 #define fb_loc 0x40002800
 
 extern "C" void start() {
+    print("scanning\n");
+
+    pci_scan();
+
+    print("done\n");
+    
     QemuRamFBCfg ramfb_cfg = {
         .address = BE64(fb_loc),
         .fourcc = BE32(FORMAT_XRGB8888),
@@ -16,8 +27,15 @@ extern "C" void start() {
     
     qemu_ramfb_configure(&ramfb_cfg);
 
-    uint32_t* fb_address = (uint32_t*)fb_loc;
-    for (int i = 0; i < WIDTH*HEIGHT; i++) {
-        fb_address[i] = 0xFFFF0000;
+    uint32_t* fb = (uint32_t*)fb_loc;
+
+    while (1)
+    {
+        for (int i = 0; i < WIDTH * HEIGHT; i++)
+            fb[i] = 0xFF000000;
+
+        draw_text(fb, "Hello OS!", 100, 100, 0xFFFFFFFF, 2);
+
+        for (volatile int i = 0; i < 1000000; i++);
     }
 }
