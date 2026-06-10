58f40bae1238f77c2ec0cce789d4540276853162
Jeremy <slurpchunk7@gmail.com>
2026-06-10 22:17:04 +1000

    Added memory utils

---
+++ src/utils/mem.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void* memcpy(void* dst, const void* src, size_t n) {
+    uint8_t* d = (uint8_t*)dst;
+    const uint8_t* s = (const uint8_t*)src;
+    for (size_t i = 0; i < n; i++) d[i] = s[i];
+    return dst;
+}
+
+static inline void* memset(void* dst, int val, size_t n) {
+    uint8_t* d = (uint8_t*)dst;
+    for (size_t i = 0; i < n; i++) d[i] = (uint8_t)val;
+    return dst;
+}
+
+static inline int memcmp(const void* a, const void* b, size_t n) {
+    const uint8_t* pa = (const uint8_t*)a;
+    const uint8_t* pb = (const uint8_t*)b;
+    for (size_t i = 0; i < n; i++) {
+        if (pa[i] != pb[i]) return pa[i] - pb[i];
+    }
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
